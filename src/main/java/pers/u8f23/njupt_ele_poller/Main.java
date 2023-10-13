d9861202ad99a45bbe1c1817f5d7c9fa1b9240b5
8f23 <calcite@139.com>
2023-10-13 23:14:26 +0800

    init

---
+++ src/main/java/pers/u8f23/njupt_ele_poller/Main.java
@@ -0,0 +1,18 @@
+package pers.u8f23.njupt_ele_poller;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 入口主函数。
+ *
+ * @author 8f23
+ * @create 2023/4/15-11:57
+ */
+@Slf4j
+public class Main
+{
+	public static void main(String[] args)
+	{
+		log.info("Hello World!");
+	}
+}
